1ea94da925b893acb21a159893c8aad92531ec77
Tanyamoki <tanya.moukoko@saintjeanmanagement.org>
2025-01-23 09:01:57 +0100

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -7,5 +7,4 @@ def predict ():
     data = request.get_json()
     prediction = model.predict([data['features']])
     return jsonify({'prediction': int (prediction [0])})
-  if __name__ == '__main__':
-    app.run (debug=True)
+app.run (debug=True)
